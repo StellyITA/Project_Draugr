29101acfd9f10ae3c312c685dfb959f6f5ac23b4
Stelly <stellyita@gmail.com>
2024-10-19 16:54:58 +0200

    fix: include Scripts.meta

---
+++ Assets/Scripts/DiceRoll.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using static System.String;
+
+public class DiceRoll : MonoBehaviour
+{
+    public int _value { get; private set; }
+    private TextMeshProUGUI _text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+	_text = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	// If player presses spacebar
+	if (Input.GetKeyDown(KeyCode.Space))
+	{
+	    Debug.Log(_text);
+	    // public static int Range(int minInclusive, int maxExclusive);
+	    _value = Random.Range(1,7);
+	    _text.text = Format("{0}", _value);
+	}
+    }
+}
