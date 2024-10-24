dfd83f6712a650a0735ff73353c253748a892179
Stelly <stellyita@gmail.com>
2024-10-24 10:20:01 +0200

    fix: edit the prefabs

--- Assets/Scripts/DiceRoll.cs
+++ Assets/Scripts/DiceRoll.cs
@@ -1,29 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 using static System.String;
 
 public class DiceRoll : MonoBehaviour
 {
-    public int _value { get; private set; }
-    private TextMeshProUGUI _text;
+	public int _value { get; private set; }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-	_text = GetComponent<TextMeshProUGUI>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-	// If player presses spacebar
-	if (Input.GetKeyDown(KeyCode.Space))
+	void Awake()
 	{
-	    // public static int Range(int minInclusive, int maxExclusive);
-	    _value = Random.Range(1,7);
-	    _text.text = Format("{0}", _value);
+		// UnityEngine.Random
+		// public static int Range(int minInclusive, int maxExclusive)
+		_value = Random.Range(1,7);
 	}
-    }
 }
